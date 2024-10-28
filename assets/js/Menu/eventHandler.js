eee2af208e4c134650418d3837648e232610c2f7
jarher <jefferh33@gmail.com>
2024-10-27 21:03:13 -0500

    refactoring js code

--- assets/js/Menu/eventHandler.js
+++ assets/js/Menu/eventHandler.js
@@ -1,8 +1,25 @@
-import { EventListener } from "./eventListener.js";
-
 const eventHandler = {
   listener: (elementsToAttachEvent, DOMHandler) =>
     new EventListener(elementsToAttachEvent, DOMHandler),
 };
 
+class EventListener {
+  constructor(elementsToAttachEvent, DOMHandler) {
+    this.elementsToAttachEvent = elementsToAttachEvent;
+    this.DOMHandler = DOMHandler;
+    this.listen();
+  }
+
+  listen() {
+    if (this.elementsToAttachEvent.hasOwnProperty("clickEvent")) {
+      const { elementsGroup } = this.elementsToAttachEvent.clickEvent;
+
+      elementsGroup.forEach((elementParams) => {
+        const { selector, method } = elementParams;
+        const DOMElement = this.DOMHandler.getElement(selector);
+        DOMElement.addEventListener("click", () => method());
+      });
+    }
+  }
+}
 export default eventHandler;
